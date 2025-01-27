79764b620a7320241949b0e37d0522f89d41cab1
Victor Coda <magma.github@gmail.com>
2025-01-27 02:07:48 +0200

    Removed unused quadric library. Removed pthread option.

--- Makeshared.mk
+++ Makeshared.mk
@@ -4,20 +4,18 @@ GLSLC=$(VULKAN_SDK)/bin/glslangValidator
 PLATFORM=VK_USE_PLATFORM_XCB_KHR
 THIRD_PARTY=../third-party
 INCLUDE_DIR=-I$(VULKAN_SDK)/include -I$(THIRD_PARTY) -I$(THIRD_PARTY)/rapid
-LIB_DIR=-L$(VULKAN_SDK)/lib -L$(THIRD_PARTY)/magma -L$(THIRD_PARTY)/quadric
+LIB_DIR=-L$(VULKAN_SDK)/lib -L$(THIRD_PARTY)/magma
 
-BASE_CFLAGS=-std=c++17 -m64 -msse4 -pthread -MD -D$(PLATFORM) $(INCLUDE_DIR)
+BASE_CFLAGS=-std=c++17 -m64 -msse4 -MD -D$(PLATFORM) $(INCLUDE_DIR)
 DEBUG ?= 1
 ifeq ($(DEBUG), 1)
 	CFLAGS=$(BASE_CFLAGS) -O0 -g -D_DEBUG
 	MAGMA=magmad
-	QUADRIC=quadricd
 else
 	CFLAGS=$(BASE_CFLAGS) -O3 -DNDEBUG
 	MAGMA=magma
-	QUADRIC=quadric
 endif
-LDFLAGS=$(LIB_DIR) -l$(MAGMA) -lpthread -lxcb -lvulkan
+LDFLAGS=$(LIB_DIR) -l$(MAGMA) -lxcb -lvulkan
 
 FRAMEWORK=../framework
 FRAMEWORK_OBJS= \
